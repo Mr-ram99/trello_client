c9c40bbe0a2f55d444b96fa23fcfc691fc0852b8
Jabra Ram <jrchoudhary2410@gmail.com>
2024-07-29 00:45:50 +0530

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
