c674baf1ee092f4f4af39cdb45a1a475b16efcbb
Jabra Ram <jrchoudhary2410@gmail.com>
2024-07-31 23:06:50 +0530

    make side panel as client component

--- src/app/components/SidePanel/index.tsx
+++ src/app/components/SidePanel/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import { Button, Flex, Input } from "antd";
 import {
@@ -24,6 +25,16 @@ interface SidePanelProps {
 }
 
 export default function SidePanel({ isOpen, setClose }: SidePanelProps) {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null;
+  }
+
   const statusOption = [
     { value: "pending", label: "Pending" },
     { value: "progress", label: "Progress" },
